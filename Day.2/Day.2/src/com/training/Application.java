cae1d41c145533b3dfa1d2b5acca3661ce1c9f62
VarshaTanwar <66243883+VarshaTanwar@users.noreply.github.com>
2020-09-16 15:46:02 +0530

    Add files via upload

--- Day.2/Day.2/src/com/training/Application.java
+++ Day.2/Day.2/src/com/training/Application.java
@@ -3,29 +3,11 @@ package com.training;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Product tv = new Product(1010,"LED TV",23000.0,1,4,0.2);
-		Product fridge = new Product(1020,"FRIDGE",20000.0,3,3,0.25);
-		
-		ManageProduct mgr = new ManageProduct();
-		
-		
-		double discountPercentage = mgr.findPercentage(tv);
-		
-		System.out.println("discount Percentage - > "+ discountPercentage);
-		
-		Product[] productList = new Product[2];
-		
-		productList[0]= tv;
-		productList[1]= fridge;
-		mgr.calculateDiscount(productList);
-		
-		mgr.showProductForEach(productList);
-		mgr.showProductsLegacy(productList);
-		
-		
-		
+		Insurance client1 = new Insurance(101,"Joey",27,10000.0);
+		 
+		double premiumAmount = client1.calculatePremium();
+		System.out.println("The premium amount for" + client1.getPolicyHolderName()+ " is " + premiumAmount);
+
 	}
 
 }
